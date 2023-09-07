29f422fa6b9d6ad6e2fd5317351d2d416fc71051
Salman <therodfighter@gmail.com>
2023-09-08 01:27:48 +0530

    sasa

--- server/src/asset/dto/create-asset.input.ts
+++ server/src/asset/dto/create-asset.input.ts
@@ -1,9 +1,8 @@
 import { InputType, Field, Int } from '@nestjs/graphql';
 import { Asset } from '@prisma/client';
 import { IsNotEmpty } from 'class-validator';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-ignore
-import { FileUpload, GraphQLUpload } from 'graphql-upload';
+import GraphQLUpload from 'graphql-upload/GraphQLUpload.mjs';
+import Upload from 'graphql-upload/Upload.mjs';
 
 @InputType()
 export class CreateAssetInput implements Omit<Asset, 'id' | 'url'> {
@@ -13,7 +12,8 @@ export class CreateAssetInput implements Omit<Asset, 'id' | 'url'> {
 
   @Field(() => GraphQLUpload)
   @IsNotEmpty()
-  files: Promise<FileUpload>[];
+  files: Promise<Upload>[];
+
   @Field(() => Int, { nullable: true })
   userId: number | null;
 
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationError } from 'class-validator';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import { graphqlUploadExpress } from 'graphql-upload';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -19,17 +15,6 @@ async function bootstrap() {
       },
     }),
   );
-
-  app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
-
-  const config = new DocumentBuilder()
-    .setTitle('Project Reviews')
-    .setDescription('The Project Review API')
-    .setVersion('0.1')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/swagger', app, document);
 
   await app.listen(3000);
 }
