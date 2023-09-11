44c29310b020dd51599f8a3feffe0098e1d0f32c
salman <therodfighter@gmail.com>
2023-09-11 04:50:20 +0400

    dfd

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,12 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  modules: [
-    "nuxt-icon",
-    "@nuxtjs/apollo",
-    "@pinia/nuxt",
-    "@invictus.codes/nuxt-vuetify",
-  ],
+  devtools: { enabled: false },
+  modules: ["nuxt-icon", "@nuxtjs/apollo", "@pinia/nuxt"],
   apollo: {
     autoImports: true,
     clients: {
@@ -15,25 +10,11 @@ export default defineNuxtConfig({
       },
     },
   },
-  vuetify: {
-    /* vuetify options */
-    vuetifyOptions: {
-      // @TODO: list all vuetify options
-    },
-
-    moduleOptions: {
-      /* nuxt-vuetify module options */
-      treeshaking: true,
-      useIconCDN: false,
-
-      /* vite-plugin-vuetify options */
-      styles: true,
-      autoImport: true,
-      useVuetifyLabs: true,
-    },
-  },
   runtimeConfig: {
     baseApi: "http://localhost:3000/graphql",
   },
-  css: ["~/assets/css/main.css"],
+  css: ["vuetify/lib/styles/main.sass"],
+  build: {
+    transpile: ["vuetify"],
+  },
 });
---
+++ client/pages/admin/forgot-password.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+import { Icon } from "#components";
+const visible = useState("visible", () => false);
+const confirmVisible = useState("confirmVisible", () => false);
+const EyeOpenIcon = h(Icon, {
+  name: "material-symbols:visibility-outline-rounded",
+});
+const EyeCloseIcon = h(Icon, {
+  name: "material-symbols:visibility-off-outline",
+});
+</script>
+
+<template>
+  <div
+    class="p-2 centered"
+    color="secondary"
+    style="
+      background-image: url('/images/login-bg.png');
+      background-position: center;
+      background-color: #f5f0ea;
+      position: relative;
+      height: 100vh;
+    "
+  >
+    <v-card
+      class="mx-auto pa-6 pb-8"
+      elevation="2"
+      max-width="448"
+      max-height="600"
+      rounded="lg"
+      style="top: 50%; transform: translateY(-50%)"
+    >
+      <BrandLogo />
+      <v-card-text class="text-h5 text-center">Reset your password</v-card-text>
+      <v-card-text class="text-caption">
+        Please choose a new password
+      </v-card-text>
+      <div class="text-subtitle-1 text-medium-emphasis">New Password</div>
+      <v-text-field
+        :append-inner-icon="visible ? EyeOpenIcon : EyeCloseIcon"
+        :type="visible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Enter new password"
+        variant="outlined"
+        @click:append-inner="visible = !visible"
+      ></v-text-field>
+
+      <div
+        class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between"
+      >
+        Confirm New Password
+      </div>
+
+      <v-text-field
+        :append-inner-icon="confirmVisible ? EyeOpenIcon : EyeCloseIcon"
+        :type="confirmVisible ? 'text' : 'password'"
+        density="compact"
+        placeholder="Confirm new password"
+        variant="outlined"
+        @click:append-inner="confirmVisible = !confirmVisible"
+      ></v-text-field>
+
+      <v-btn block class="mb-8" size="large" color="primary">
+        Reset Password
+      </v-btn>
+    </v-card>
+  </div>
+</template>
